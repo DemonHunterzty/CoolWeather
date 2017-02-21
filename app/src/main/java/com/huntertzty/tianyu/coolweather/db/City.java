f7b79d38b80383d1e5b8634a159b92ce2428c027
HunterZty <hunterztyjobs@gmail.com>
2017-02-21 22:11:36 +0800

    db configuration and creation

---
+++ app/src/main/java/com/huntertzty/tianyu/coolweather/db/City.java
@@ -0,0 +1,52 @@
+package com.huntertzty.tianyu.coolweather.db;
+
+/**
+ * Created by Tianyu on 2/21/2017.
+ */
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+
+    private int id;
+
+    private String cityName;
+
+    private int cityCode;
+
+    private int provinceId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
+
